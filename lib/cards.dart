9eb91ffd1bec6617d8d8e67026fbc2b21857fee1
Jonathan Cowling <jonathan.cowling@moneysupermarket.com>
2019-06-18 17:36:21 +0100

    some fluro stuff (WIP)

--- lib/cards.dart
+++ lib/cards.dart
@@ -1,7 +1,6 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:fluro/fluro.dart' as fluro;
 
 class Page extends StatefulWidget {
   @override
@@ -13,12 +12,19 @@ class Page extends StatefulWidget {
 class _PageState extends State<Page> {
   @override
   Widget build(BuildContext context) {
-    return Router({
-      "/": Container(color: Colors.red, padding: EdgeInsets.all(0)),
-      "/test": Container(color: Colors.green, padding: EdgeInsets.all(30)),
-      "/test/a": Container(color: Colors.yellow, padding: EdgeInsets.all(50)),
-      "/test/abc": Container(color: Colors.blue, padding: EdgeInsets.all(60)),
-      "/logo": FlutterLogo()
+
+    return Flow({
+      "/": (_) => Padding(padding: EdgeInsets.all(0), child: Container(color: Colors.red)),
+      "/test": (_) => Padding(padding: EdgeInsets.all(10), child: Container(color: Colors.green)),
+      "/test/a": (ctx) => Padding(padding: EdgeInsets.all(0),
+          child: Container(color: Colors.yellow, child: GestureDetector(onTap: (){
+            Flow.of(ctx).pop();
+          }))),
+      "/test/abc": (ctx) => Padding(padding: EdgeInsets.all(30),
+          child: Container(color: Colors.blue, child: GestureDetector(onTap: (){
+            Flow.of(ctx).navigateTo("/test/a");
+          }))),
+      "/logo": (_) => FlutterLogo()
     },
         "/test/abc",
         "/logo"
@@ -26,46 +32,57 @@ class _PageState extends State<Page> {
   }
 }
 
-class Router extends StatefulWidget {
-  final Map<String, Widget> routes;
+class Flow extends StatefulWidget {
+  final Map<String, Widget Function(BuildContext)> routes;
   final String initial;
   final String unknown;
   
-  Router(this.routes, this.initial, this.unknown);
+  Flow(this.routes, this.initial, this.unknown);
+
+  static FlowState of(BuildContext ctx) {
+    return ctx.ancestorStateOfType(TypeMatcher<FlowState>());
+  }
   
   @override
   State<StatefulWidget> createState() {
-    return _RouterState(routes, initial, unknown);
+    return FlowState(routes, initial, unknown);
   }
 }
 
-class _RouterState extends State<Router> {
-  final Map<String, Widget> routes;
-  final String initial;
-  final String unknown;
+class FlowState extends State<Flow> {
+  final Map<String, Widget Function(BuildContext)> _routes;
+  final String _initial;
+  final String _unknown;
+  final fluro.Router _router;
 
-  _RouterState(this.routes, this.initial, this.unknown);
+  FlowState._create(this._routes, this._initial, this._unknown, this._router);
+
+  void navigateTo(String path) {
+    _router.navigateTo(context, path);
+  }
+  
+  void pop() {
+    _router.pop(context);
+  }
+  
+  factory FlowState(Map<String, Widget Function(BuildContext)> routes, String initial, String unknown) {
+    final router = fluro.Router();
+    routes.forEach((route, widget){
+      router.define(route, handler: fluro.Handler(handlerFunc: (ctx, args) => widget(ctx)));
+    });
+
+    return FlowState._create(routes, initial, unknown, router);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(child: Navigator(
-        initialRoute: initial,
+        initialRoute: _initial,
+        onGenerateRoute: _router.generator,
         onUnknownRoute: (settings) => PageRouteBuilder(
             settings: settings,
-            pageBuilder: (ctx, animIn, animOut) => routes[unknown]),
-        onGenerateRoute: (settings) {
-            return PageRouteBuilder(
-                settings: settings,
-                pageBuilder: (ctx, animIn, animOut) {
-                  var route = routes[settings.name];
-                  print(settings);
-                  if (route == null) {
-                    return routes[unknown];
-                  }
-                  return route;
-                }
-            );
-        })
+            pageBuilder: (ctx, animIn, animOut) => _routes[_unknown](ctx)),
+        )
     );
   }
   
